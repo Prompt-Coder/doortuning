7214edb5f0ae0b51d71feeb5863148d8e3ddf2a0
Prompt-Coder <65853453+Prompt-Coder@users.noreply.github.com>
2025-01-22 14:22:18 -0800

    Add files via upload

---
+++ prompt_doortuning/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game "gta5"
+
+replace_level_meta 'gta5'
+
+files {
+	'gta5.meta',
+	'doortuning.ymt'
+	--'water.xml'
+}
+
+--data_file 'HANDLING_FILE' 'water.xml'
+
+escrow_ignore {
+  'stream/*.ymt'
+}
